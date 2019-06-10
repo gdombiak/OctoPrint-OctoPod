577f452634e289879f80462831cde398f3ebb0ea
Gaston Dombiak <gdombiak@gmail.com>
2019-06-09 21:45:17 -0700

    Use #startswith instead of compare full string since lines may have \n. Issue #2

--- octoprint_octopod/mmu.py
+++ octoprint_octopod/mmu.py
@@ -13,7 +13,7 @@ class MMUAssistance:
 		# Firmware will use 2 different lines to indicate the there is an MMU issue
 		# and user assistance is required. There could be other lines present in the
 		# terminal between the 2 relevant lines
-		if line == "mmu_get_response - begin move: T-code":
+		if line.startswith("mmu_get_response - begin move: T-code"):
 			self._mmu_lines_skipped = 0
 		else:
 			if self._mmu_lines_skipped is not None:
@@ -21,7 +21,7 @@ class MMUAssistance:
 					# We give up waiting for the second line to be detected. False alert.
 					# Reset counter
 					self._mmu_lines_skipped = None
-				elif line == "mmu_get_response() returning: 0":
+				elif line.startswith("mmu_get_response() returning: 0"):
 					# Check if we never alerted or 1 minute has passed since last alert
 					if self._last_notification is None or (time.time() - self._last_notification) / 60 > 1:
 						self._logger.info("*** MMU Requires User Assistance ***")
