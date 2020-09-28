252060ea5d66cca1e4daafcec746d163af5eb4dd
Gaston Dombiak <gdombiak@gmail.com>
2020-09-27 23:22:41 -0700

    Updated release to 0.2.8

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ plugin_package = "octoprint_octopod"
 plugin_name = "OctoPrint-OctoPod"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "0.2.7"
+plugin_version = "0.2.8"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
