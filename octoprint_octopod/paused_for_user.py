569df025218836339d36ba22196ad5082811dcd3
Ivan Dombiak <jivesdope@gmail.com>
2022-08-06 17:50:03 -0700

    Send pause notification for M25 gcode.
    Issue #153

--- octoprint_octopod/paused_for_user.py
+++ octoprint_octopod/paused_for_user.py
@@ -16,7 +16,7 @@ class PausedForUser(BaseNotification):
 		# print to terminal 'paused for user' message so this will help for those cases
 		# Prusa firmware supports M601 that is not supported by Marlin
 		if gcode:
-			if gcode == "M600" or gcode == "M601":
+			if gcode == "M600" or gcode == "M601" or gcode == "M25":
 				# Check if this type of notification is disabled
 				if not self.__is_notification_enabled(settings):
 					return
